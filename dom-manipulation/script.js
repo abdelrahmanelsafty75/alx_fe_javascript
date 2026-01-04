154cbe8a90ac881dff25671546acab7ef27bbc85
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-01-04 22:43:44 +0200

    adding displayRandomQoute function

---
+++ dom-manipulation/script.js
@@ -0,0 +1,27 @@
+const dsiplayDiv = document.getElementById('quoteDisplay');
+const showQuoteBtn = document.getElementById('newQuote');
+const inputQuoteText = document.getElementById('newQuoteText');
+const inputQuoteCategory = document.getElementById('newQuoteCategory');
+
+let quotes = [
+            { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Inspiration" },
+            { text: "Do what you can, with what you have, where you are.", category: "Motivation" },
+            { text: "Code is like humor. When you have to explain it, it’s bad.", category: "Programming" }
+        ];
+
+function displayRandomQuote() {
+    dsiplayDiv.innerHTML = '';
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+
+    const qoutElementText = document.createElement('p');
+    qoutElementText.textContent = `"${quote.text}"`;
+
+    const qoutElemtnCategory = document.createElement('p');
+    qoutElemtnCategory.textContent = `- ${quote.category}`;
+
+    dsiplayDiv.appendChild(qoutElementText);
+    dsiplayDiv.appendChild(qoutElemtnCategory);
+}
+
+
